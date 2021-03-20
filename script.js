eb8326e6efd9a353643ff26e36f1a6c42038fc63
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-20 22:04:55 +0000

    Assignment-4-1

---
+++ script.js
@@ -0,0 +1,70 @@
+document.getElementById('b1').addEventListener('click', change)
+
+function change () {
+  document.getElementById('paragraph').innerHTML = '<p>11. NeXT Computers<br><br>On that same day, he founded NeXT  with the intention of competing with Apple, though that plan didn’t work out, the company did produce an operating system and Apple bought NeXT for $400 million in 1996 (Kahney). NeXT Computer released it’s first computer in 1988, in 1993 it discontinued it’s hardware business and got into software instead. (CNET News Staff).</p>'
+  document.getElementById('right-img').src = './Images/11. NeXT Computers.JPG'
+}
+// --------------------------1
+document.getElementById('b2').addEventListener('click', change1)
+
+function change1 () {
+  document.getElementById('paragraph').innerHTML = '<p>12. Pixar Animation Studios<br><br>“In 1986, Jobs purchased an animation company from George Lucas, which later became Pixar Animation Studios. Believing in Pixar`s potential, Jobs initially invested $50 million of his own money in the company” (Biography.com). While it was initially just a computer graphics group, he launched Pixar with John Lasseter in 1986 (Fekete). Lesseter said about Jobs that “[h]e went through an arc in his life. There was a time the way he worked with people was not good, and I saw that when I first worked with him. But peo­ple look at that dramatic part, and they’ll make a movie about that — and that’s not the story. That was the beginning of a more interesting and complex story because when he left Apple, he then entered into what really is the classic hero’s journey: He’s wandering in the wilderness, he’s working with NeXT, it’s not working. He’s working with Pixar, we’re failing. In that process, Steve learned some major lessons, and he changed. He became an empathetic person” (Fekete). Together they made the “first fully computer-animated feature film”(Wikepedia) Toy Story in 1995.</p>'
+  document.getElementById('right-img').src = './Images/12. Pixar Animation Studios.JPG'
+}
+// --------------------------2
+document.getElementById('b3').addEventListener('click', change2)
+
+function change2 () {
+  document.getElementById('paragraph').innerHTML = '<p>13. Family <br><br>“Jobs and Laurene Powell married on March 18, 1991[they had] three children, Reed, Erin, and Eve” (Biography.com).</p>'
+  document.getElementById('right-img').src = './Images/13. Family .JPG'
+}
+// --------------------------3
+document.getElementById('b4').addEventListener('click', change3)
+
+function change3 () {
+  document.getElementById('paragraph').innerHTML = '<p>14. Competition with Bill Gates and Microsoft<br><br>In 1996 Jobs and Gates starred in the documentary “Triumph of the Nerds” about the development of the personal computer. Jobs “ripped into Gates and Microsoft, saying that they made “third-rate products” (Hartmans, Weinberger).</p>'
+  document.getElementById('right-img').src = './Images/14. Competition with Bill Gates and Microsoft.JPG'
+}
+// --------------------------4
+document.getElementById('b5').addEventListener('click', change4)
+
+function change4 () {
+  document.getElementById('paragraph').innerHTML = '<p>15. Return to Apple<br><br>On the exact same date that Jobs left Apple, 11 years later, “September 16th, 1997, Jobs returned to Apple” (Kahney). “then-Apple CEO Gil Amelio negotiated a deal to buy NeXT, the computer startup operated by an exiled Jobs, in hopes that he would bring some much-needed direction to the company, [which was struggling financially] “Instead, Jobs staged a boardroom coup that resulted in Amelio`s resignation” (Weinberger, Hartmans). Gates tried to talk Amelio out of bringing Jobs back to Apple (Harmans, Weinberger).</p>'
+  document.getElementById('right-img').src = './Images/15. Return to Apple.JPG'
+}
+// --------------------------5
+document.getElementById('b6').addEventListener('click', change5)
+
+function change5 () {
+  document.getElementById('paragraph').innerHTML = '<p>16. Apple is Profitable Again<br><br>“In August 1997, Jobs […] announce[s] that Apple had taken a $150 million investment from its long-time rivals at Microsoft. "We need all the help we can get," Jobs said, to boos from the audience […] [however in “early 1998, at yet another Macworld Expo […] [t]hanks to Jobs` product direction and Microsoft`s help, Apple was finally profitable again” (Weinberger, Hartmans). The Apple Store was introduced this year (CNET News Staff).</p>'
+  document.getElementById('right-img').src = './Images/16. Apple is Profitable Again.JPG'
+}
+// --------------------------6
+document.getElementById('b7').addEventListener('click', change6)
+
+function change6 () {
+  document.getElementById('paragraph').innerHTML = '<p>17. iMac<br><br>In 1998, Ken Segall came up with the name iMac, rather than the name MacMan that Jobs was leaning towards. “The I stands for Internet, Individuality and Innovation. Later Apple “adopted the ‘I’ prefix across its consumer hardware and software lines,such as iPod, iBook (later MacBook), iPhone, iPad and various pieces of software such as the iLife suite and iWork and the company’s media player/store, iTunes” (Wikipedia).</p>'
+  document.getElementById('right-img').src = './Images/17. iMac.JPG'
+}
+// --------------------------7
+document.getElementById('b8').addEventListener('click', change7)
+
+function change7 () {
+  document.getElementById('paragraph').innerHTML = '<p>18. NeXT Technologies<br><br>In the year 2000 “[t]echnologies developed at NeXT ultimately evolve into Apple products such as the Mac OS” (CNET News Staff). In 2001 the first iPod was introduced.</p>'
+  document.getElementById('right-img').src = './Images/18. NeXT Technologies.JPG'
+}
+// --------------------------8
+document.getElementById('b9').addEventListener('click', change8)
+
+function change8 () {
+  document.getElementById('paragraph').innerHTML = '<p>19. Cancer<br><br>“August 1st, 2004: Jobs undergoes surgery to remove a cancerous tumor in his pancreas” (CNET News Staff).</p>'
+  document.getElementById('right-img').src = './Images/19. Cancer.JPG'
+}
+// --------------------------9
+document.getElementById('b10').addEventListener('click', change9)
+
+function change9 () {
+  document.getElementById('paragraph').innerHTML = '<p>20. Ad-War<br><br>From 2006 to 2009 Mac and PC engaged in an ‘ad-war’. Apples 2006 “Get a Mac” campaign started an ad-war that lasted until 2009 (Kolowich). </p>'
+  document.getElementById('right-img').src = './Images/20. Ad-War.JPG'
+}
+// --------------------------10
